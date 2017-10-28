aeb7216e0e890ebd01c3538aec5615560b788b43
Hack Reactor Student <student@hackreactor.com>
2017-10-28 14:29:43 -0700

    working with live data

--- src/components/app.js
+++ src/components/app.js
@@ -3,14 +3,16 @@ angular.module('video-player')
   .component('app', {
     templateUrl: 'src/templates/app.html',
     
-    controller: function(search) {  
-      search.searchYouTube();    
+    controller: function(search) {     
       this.videos = window.exampleVideoData;
       this.currentVideo = this.videos[0];
       this.changeVideo = (video) => {
         console.log(video);
         this.currentVideo = video;
       };
-      //this.search = search;
+      search.searchYouTube('cats', (response) => {
+        this.videos = response;
+        this.currentVideo = this.videos[0];
+      }); 
     }
   });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,12 +1,12 @@
 angular.module('video-player')
   .service('search', function($http) {
-    this.searchYouTube = function(options) {
+    this.searchYouTube = function(query, cb) {
       $http({
         method: 'GET',
         url: 'https://www.googleapis.com/youtube/v3/search',
         params: {
           part: 'snippet',
-          q: 'cats',
+          q: query,
           maxResults: 5, 
           key: window.YOUTUBE_API_KEY,
           videoEmbeddable: true,
@@ -17,6 +17,7 @@ angular.module('video-player')
         // }
       }).then(function(response) {
         console.log('thanks vinoj its working', response);
+        cb(response.data.items);
       });
     };
   });
