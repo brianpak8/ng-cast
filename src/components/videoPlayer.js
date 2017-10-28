c27e4c8a7a1c3a00bca505fcfcd5e9790937596c
Hack Reactor Student <student@hackreactor.com>
2017-10-27 18:43:37 -0700

    player renders example data

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,6 +1,11 @@
 angular.module('video-player')
 
   .component('videoPlayer', {
-    templateUrl: 'src/templates/videoPlayer.html'
-    // TODO
+    templateUrl: 'src/templates/videoPlayer.html',
+    bindings: {
+      video: '<'
+    },
+    controller: function() {
+      
+    }
   });
